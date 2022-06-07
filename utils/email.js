d805db8eaab30f16a85f8dc223d1407d2d8ec0d7
Peter Njuguna <njugunapm23@gmail.com>
2022-06-07 20:31:16 +0300

    Password reset url fix

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async (options) => {
   });
   // 2) Define the email options
   const mailOptions = {
-    from: 'Peter Smart <testuser@email.io>',
+    from: 'Peter Codes <peter@codes.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
